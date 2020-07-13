9ad2ca3f55e2fbd2b3209d2ed053011841d45986
CleoMugs <cleophas.mugeni@gmail.com>
2020-07-13 18:25:33 +0300

    added toggler

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -1,28 +1,19 @@
 import React, { useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBars } from '@fortawesome/free-solid-svg-icons'
+import { useTransition, animated } from 'react-spring'
+
 
 function Navigation(){
 	const [showMenu, setShowMenu] = useState(false)
 
-	let menu 
-	let menuMask
-
-	if(showMenu){
-		menu = 
-		<div
-			className="fixed bg-white top-0 left-0 w-4/5 h-full z-50 shadow"
-		>
-			The menu
-		</div>
-
-		menuMask = 
-		<div
-			className="bg-black-t-50 fixed top-0 left-0 w-full h-full z-50"
-			onClick={() => setShowMenu(false)}
-		>
-		</div>
-	}
+	const transitions = useTransition(showMenu, null, {
+		from: { position: 'absolute', opacity: 0},
+		enter: { opacity: 1 },
+		leave: { opacity: 0 },	
+	})
+	//className="fixed bg-white top-0 left-0 w-4/5 h-full z-50 shadow"
+	// mask className="bg-black-t-50 fixed top-0 left-0 w-full h-full z-50"
 
 
 	return (
@@ -34,9 +25,33 @@ function Navigation(){
 				/>
 			</span>
 
-			{ menuMask }
+			{
+				transitions.map(({ item, key, props })  => 
+					item && 
+					<animated.div 
+						key={key} 
+						style={props}
+						className="bg-black-t-50 fixed top-0 left-0 w-full h-full z-50"
+						onClick={() => setShowMenu(false)}
+					>
+					</animated.div>
+				)
 
-			{ menu }
+			}
+
+			{
+				transitions.map(({ item, key, props })  => 
+					item && 
+					<animated.div 
+						key={key} 
+						style={props}
+						className="fixed bg-white top-0 left-0 w-4/5 h-full z-50 shadow"
+					>
+						This is the menu
+					</animated.div>
+				)
+
+			}
 
 		</nav>
 	)
