e4a7af14b8e8bb82804fee6910fe8a5a65fb2d02
CleoMugs <cleophas.mugeni@gmail.com>
2020-07-13 17:45:44 +0300

    added toggler

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -6,10 +6,21 @@ function Navigation(){
 	const [showMenu, setShowMenu] = useState(false)
 
 	let menu 
+	let menuMask
 
 	if(showMenu){
-		menu = <div>
+		menu = 
+		<div
+			className="fixed bg-white top-0 left-0 w-4/5 h-full z-50 shadow"
+		>
 			The menu
+		</div>
+
+		menuMask = 
+		<div
+			className="bg-black-t-50 fixed top-0 left-0 w-full h-full z-50"
+			onClick={() => setShowMenu(false)}
+		>
 		</div>
 	}
 
@@ -22,6 +33,8 @@ function Navigation(){
 					onClick={() => setShowMenu(!showMenu)}
 				/>
 			</span>
+
+			{ menuMask }
 
 			{ menu }
 
