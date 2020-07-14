1db09e91121ae2715d03c938986bc96fc134fd6b
CleoMugs <cleophas.mugeni@gmail.com>
2020-07-14 10:39:09 +0300

    added product-page

--- src/Views/Product.js
+++ src/Views/Product.js
@@ -18,8 +18,24 @@ function Product(){
 
 	if(product){
 		return (
+			content =
 			<div>
-				<h1>{product.name}</h1>
+				<h1 className="text-2xl font-bold mb-3">
+					{product.name}
+				</h1>
+				<div>
+					<img
+						src={product.images[0].imageUrl}
+						alt={product.name}
+					/>
+				</div>
+				<div className="font-bold text-xl mb-3">
+					$ {product.price}
+				</div>
+				<div>
+					{product.description}
+				</div>
+
 			</div>
 		)
 	}
